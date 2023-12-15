83cecff66cbbc8466e0a39a628b13e8c099a7335
김진형 <trn01@naver.com>
2023-12-15 22:49:54 +0900

    ALLOWED_HOSTS 변경

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k^18#)g#i7n*r!sj5yjymdb)l_4qbi=nm%5uzml=j9vc7l+tyf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [43.202.233.224]
 
 
 # Application definition
