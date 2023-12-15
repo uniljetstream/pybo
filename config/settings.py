aa591d1abe4a2068a65d7631ae53c72ae06dbcc3
김진형 <trn01@naver.com>
2023-12-15 23:12:55 +0900

    수정1

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k^18#)g#i7n*r!sj5yjymdb)l_4qbi=nm%5uzml=j9vc7l+tyf'
+SECRET_KEY = 'django-insecure-k^18#)g#i7n*r!sj5IMDbPY)l_4qbi=nm%5unmap=j9vc7l+tyf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
