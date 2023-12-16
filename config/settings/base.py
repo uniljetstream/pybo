6ad40f28b6ff0cc19054e6fa13e0abca04a9fba4
김진형 <trn01@naver.com>
2023-12-16 15:03:28 +0900

    dlka)

--- config/settings/base.py
+++ config/settings/base.py
@@ -185,6 +185,10 @@ LOGGING = {
             'handlers': ['console', 'mail_admins', 'file'],
             'level': 'INFO',
         },
+        'pybo': {
+            'handlers':['console', 'file'],
+            'level': 'INFO',
+        },
         'django.server': {
             'handlers': ['django.server'],
             'level': 'INFO',
