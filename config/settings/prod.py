6e05d6939c4b0c043a61f25eb42e51f6b3cdbbd1
김진형 <trn01@naver.com>
2023-12-16 18:12:29 +0900

    adfas

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,7 +2,7 @@ import environ
 
 from .base import *
 
-ALLOWED_HOSTS=['43.202.231.224', 'foolish-pillow.xyz']
+ALLOWED_HOSTS = ['43.202.231.224', 'foolish-pillow.xyz']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
